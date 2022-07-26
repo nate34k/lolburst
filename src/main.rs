3ee5af1cfaf06eb99b408bf24ce8b6c529a7472c
Nathan Kraft <nate34k@gmail.com>
2022-07-26 15:49:56 -0500

    todo: strongly type ddragon data

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::{io, thread};
-use std::sync::{Arc, Mutex};
+use std::io;
 use serde_json::{Value};
 use tokio::time::{sleep, Duration};
 use log::{info, warn, error};
 use crate::champions::orianna;
-use device_query::{DeviceEvents, DeviceState, Keycode, device_state};
+use reqwest::{Response, Client};
 
 pub mod champions;
 pub mod dmg;
@@ -41,21 +40,44 @@ fn get_input(prompt: String) -> String {
     input.trim().to_string()
 }
 
+fn get_team(game_data: &Value, i: usize) -> String {
+    String::from(game_data["allPlayers"][i]["team"].as_str().unwrap())
+}
+
+fn get_opponant_team(game_data: &Value) -> Vec<String> {
+    let mut opponant_list = Vec::new();
+    for i in 0..game_data["allPlayers"].as_array().unwrap().len() {
+        if get_team(game_data, 0) == get_team(game_data, i) {
+            opponant_list.push(String::from(game_data["allPlayers"][i]["championName"].as_str().unwrap()));
+        }
+    }
+    opponant_list
+}
+
+async fn network() -> Result<Client, reqwest::Error> {
+    reqwest::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()
+}
+
+async fn request(client: &Client, url: &str) -> Result<Response, reqwest::Error> {
+    client.get(url).send().await
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = String::from("https://static.developer.riotgames.com/docs/lol/liveclientdata_sample.json");
+    let ddragon_url = String::from("http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/champion.json");
+
     let client = reqwest::Client::builder()
-        .danger_accept_invalid_certs(true)
-        .build()?;
+        .danger_accept_invalid_certs(true).build()?;
 
+    let data: Value = serde_json::from_str(&request(&client, &ddragon_url).await?.text().await?)?;
+    
     let champion = champions::match_champion("Orianna");
-    let mut mr = 0.0;
-
-    let device_state = DeviceState::new();
-    let _guard = device_state.on_key_down(move|key| {
-        mr += 0.0;
-    });
+    
     loop {
+        let mr = 50.0;
         let request = match client.get(&url).send().await {
             Ok(request) => request,
             Err(err) => {
@@ -70,10 +92,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 break;
             }
         };
+        println!("{:?}", get_team(&game_data, 0));
+        println!("{:?}", get_opponant_team(&game_data));
+        println!("{:#?}", data["data"][get_opponant_team(&game_data)[0].clone()]["stats"]);
         let ap = game_data["activePlayer"]["championStats"]["abilityPower"].as_f64().unwrap();
         let ability_ranks = AbilityRanks::new(game_data["activePlayer"]["abilities"]["Q"]["abilityLevel"].as_i64().unwrap(),
-                                                            game_data["activePlayer"]["abilities"]["W"]["abilityLevel"].as_i64().unwrap(),
-                                                            game_data["activePlayer"]["abilities"]["E"]["abilityLevel"].as_i64().unwrap(),
+        game_data["activePlayer"]["abilities"]["W"]["abilityLevel"].as_i64().unwrap(),
+        game_data["activePlayer"]["abilities"]["E"]["abilityLevel"].as_i64().unwrap(),
                                                             game_data["activePlayer"]["abilities"]["R"]["abilityLevel"].as_i64().unwrap());
 
         println!("{}'s Burst is {:.1} vs {:.0} MR.", game_data["activePlayer"]["summonerName"].as_str().unwrap().replace('"', ""),
