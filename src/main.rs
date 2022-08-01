dd73070cd6caf05f9bcf65fad2679c7de2fdf868
Nathan Kraft <nate34k@gmail.com>
2022-08-01 16:07:48 -0500

    TODO: Finish implementing TUI

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,19 @@ use reqwest::Client;
 use serde_json::Value;
 use std::env;
 use tokio::time::{sleep, Duration};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Cell, Row, Table, TableState},
+    Frame, Terminal,
+};
 
 mod active_player;
 mod all_players;
@@ -16,6 +29,7 @@ mod champions;
 mod dmg;
 mod network;
 mod utils;
+
 
 #[derive(Debug)]
 pub struct AbilityRanks {
@@ -41,15 +55,92 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env::set_var("RUST_LOG", "trace");
     dotenv::dotenv().expect("Failed to load env from .env");
     pretty_env_logger::init();
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
+    // create app and run it
+    let app = App::new();
+    let res = run_app(&mut terminal, app).await;
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+
+}
+
+
+
+struct App<'a> {
+    state: TableState,
+    items: Vec<Vec<&'a str>>,
+}
+
+impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            state: TableState::default(),
+            items: vec![
+                vec!["Row11", stringify!(5.5), "Row13"],
+                vec!["Row21", "Row22", "Row23"],
+                vec!["Row31", "Row32", "Row33"],
+                vec!["Row41", "Row42", "Row43"],
+                vec!["Row51", "Row52", "Row53"],
+            ],
+        }
+    }
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
+
+async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App<'_>) -> io::Result<()> {
     let active_player_json_locations = deserializer::JSONDataLocations {
-        url: env::var("ACTIVE_PLAYER_URL")?,
-        json: env::var("ACTIVE_PLAYER_JSON")?,
+        url: env::var("ACTIVE_PLAYER_URL").unwrap(),
+        json: env::var("ACTIVE_PLAYER_JSON").unwrap(),
     };
 
     let all_player_json_locations = deserializer::JSONDataLocations {
-        url: env::var("ALL_PLAYERS_URL")?,
-        json: env::var("ALL_PLAYERS_JSON")?,
+        url: env::var("ALL_PLAYERS_URL").unwrap(),
+        json: env::var("ALL_PLAYERS_JSON").unwrap(),
     };
 
     let client: Client = network::build_client().await;
@@ -81,12 +172,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         let (active_player_data, all_player_data) =
             deserializer::deserializer(&deserializer_params).await;
-
+    
         let opponant_team = teams::OpponantTeam::new(&active_player_data, &all_player_data);
-
+    
         let resistance =
             resistance::Resistance::new(&active_player_data, &all_player_data, &ddragon_data);
-
+    
         // Set a Vec<f64> for opponant AR values
         let mut ar = Vec::new();
         for i in 0..opponant_team.opponants.len() {
@@ -101,7 +192,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let scaled_mr = base_mr + (mr_per_level * (level - 1.0));
             ar.push(scaled_mr)
         }
-
+    
         // Other data we need to print
         let ability_ranks = AbilityRanks::new(
             active_player_data.abilities.q.ability_level,
@@ -109,7 +200,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             active_player_data.abilities.e.ability_level,
             active_player_data.abilities.r.ability_level,
         );
-
+    
         // Loop to print burst dmg against each enemy champion
         for i in 0..opponant_team.opponants.len() {
             let r = dmg::Resistance::new(resistance.armor[i], resistance.magic_resist[i]);
@@ -119,9 +210,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 opponant_team.opponants[i].0
             );
         }
-
+        app.items = vec![
+            vec!["Row11", stringify!(10.0), "Row13"],
+            vec!["Row21", "Row22", "Row23"],
+            vec!["Row31", "Row32", "Row33"],
+            vec!["Row41", "Row42", "Row43"],
+            vec!["Row51", "Row52", "Row53"],
+        ];
         println!("================================");
-
+    
         // Sleep for 5 seconds between running the loop again to save resources
         sleep(Duration::from_secs(
             env::var("SAMPLE_RATE")
@@ -130,7 +227,51 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .unwrap_or(15),
         ))
         .await;
-    }
+        terminal.draw(|f| ui(f, &mut app))?;
 
-    Ok(())
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let rects = Layout::default()
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .margin(0)
+        .split(f.size());
+
+    let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let normal_style = Style::default().bg(Color::Blue);
+    let header_cells = ["Champion", "Level", "Burst"]
+        .iter()
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+    let header = Row::new(header_cells)
+        .style(normal_style)
+        .height(1)
+        .bottom_margin(1);
+    let rows = app.items.iter().map(|item| {
+        let height = item
+            .iter()
+            .map(|content| content.chars().filter(|c| *c == '\n').count())
+            .max()
+            .unwrap_or(0)
+            + 1;
+        let cells = item.iter().map(|c| Cell::from(*c));
+        Row::new(cells).height(height as u16).bottom_margin(1)
+    });
+    let t = Table::new(rows)
+        .header(header)
+        .block(Block::default().borders(Borders::ALL).title("lolburst"))
+        .highlight_style(selected_style)
+        .highlight_symbol(">> ")
+        .widths(&[
+            Constraint::Percentage(50),
+            Constraint::Length(30),
+            Constraint::Min(10),
+        ]);
+    f.render_stateful_widget(t, rects[0], &mut app.state);
 }
