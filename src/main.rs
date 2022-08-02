9f79c43a76bdef39297a7f81e5a5b8b6fc5938d6
Nathan Kraft <nate34k@gmail.com>
2022-08-02 09:21:11 -0500

    todo: fix logging rect for tui-logger

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use crossterm::{
 use log::info;
 use reqwest::Client;
 use serde_json::Value;
+use tui_logger::{TuiLoggerWidget, TuiLoggerLevelOutput};
 use std::env;
 use std::{error::Error, io};
 use tokio::time::{sleep, Duration};
@@ -51,23 +52,23 @@ impl AbilityRanks {
     }
 }
 
-fn display_data(
-    champion: &ActiveChampion,
-    active_player_data: &active_player::Root,
-    ability_ranks: &AbilityRanks,
+fn display_data<'a>(
+    champion: &'a ActiveChampion,
+    active_player_data: active_player::Root,
+    ability_ranks: AbilityRanks,
     opponant_team: OpponantTeam,
     resistance: resistance::Resistance,
-) -> Vec<(String, f64, i64)> {
+) -> Vec<Vec<String>> {
     let mut ret = Vec::new();
     // Loop to print burst dmg against each enemy champion
     for i in 0..opponant_team.opponants.len() {
+        let mut row = Vec::new();
         let r = dmg::Resistance::new(resistance.armor[i], resistance.magic_resist[i]);
         let burst_dmg = dmg::burst_dmg(&champion, &active_player_data, &ability_ranks, r);
-        ret.push((
-            opponant_team.opponants[i].0.clone(),
-            burst_dmg,
-            opponant_team.opponants[i].1,
-        ));
+        row.push(opponant_team.opponants[i].0.clone());
+        row.push(opponant_team.opponants[i].1.to_string());
+        row.push(burst_dmg.floor().to_string());
+        ret.push(row);
     }
     ret
 }
@@ -76,7 +77,15 @@ fn display_data(
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env::set_var("RUST_LOG", "trace");
     dotenv::dotenv().expect("Failed to load env from .env");
-    pretty_env_logger::init();
+    
+    // Early initialization of the logger
+
+    // Set max_log_level to Trace
+    tui_logger::init_logger(log::LevelFilter::Trace).unwrap();
+
+    // Set default level for unknown targets to Trace
+    tui_logger::set_default_level(log::LevelFilter::Trace);
+
     // setup terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -104,21 +113,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-struct App<'a> {
+struct App {
     state: TableState,
-    items: Vec<Vec<&'a str>>,
+    items: Vec<Vec<String>>,
 }
 
-impl<'a> App<'a> {
-    fn new() -> App<'a> {
+impl App {
+    fn new() -> App {
         App {
             state: TableState::default(),
             items: vec![
-                vec!["Row11", stringify!(5.5), "Row13"],
-                vec!["Row21", "Row22", "Row23"],
-                vec!["Row31", "Row32", "Row33"],
-                vec!["Row41", "Row42", "Row43"],
-                vec!["Row51", "Row52", "Row53"],
+                vec!["Row11".to_string(), "Row12".to_string(), "Row13".to_string()],
+                vec![
+                    "Row21".to_string(),
+                    "Row22".to_string(),
+                    "Row23".to_string(),
+                ],
+                vec![
+                    "Row31".to_string(),
+                    "Row32".to_string(),
+                    "Row33".to_string(),
+                ],
+                vec![
+                    "Row41".to_string(),
+                    "Row42".to_string(),
+                    "Row43".to_string(),
+                ],
+                vec![
+                    "Row51".to_string(),
+                    "Row52".to_string(),
+                    "Row53".to_string(),
+                ],
             ],
         }
     }
@@ -151,7 +176,7 @@ impl<'a> App<'a> {
     }
 }
 
-async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App<'_>) -> io::Result<()> {
+async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
     let active_player_json_locations = deserializer::JSONDataLocations {
         url: env::var("ACTIVE_PLAYER_URL").unwrap(),
         json: env::var("ACTIVE_PLAYER_JSON").unwrap(),
@@ -197,21 +222,6 @@ async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App<'_>) -> io
         let resistance =
             resistance::Resistance::new(&active_player_data, &all_player_data, &ddragon_data);
 
-        // Set a Vec<f64> for opponant AR values
-        let mut ar = Vec::new();
-        for i in 0..opponant_team.opponants.len() {
-            let champion_name = &opponant_team.opponants[i].0;
-            let base_mr = ddragon_data["data"][champion_name]["stats"]["armor"]
-                .as_f64()
-                .unwrap();
-            let mr_per_level = ddragon_data["data"][champion_name]["stats"]["armorperlevel"]
-                .as_f64()
-                .unwrap();
-            let level = opponant_team.opponants[i].1 as f64;
-            let scaled_mr = base_mr + (mr_per_level * (level - 1.0));
-            ar.push(scaled_mr)
-        }
-
         // Other data we need to print
         let ability_ranks = AbilityRanks::new(
             active_player_data.abilities.q.ability_level,
@@ -220,25 +230,15 @@ async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App<'_>) -> io
             active_player_data.abilities.r.ability_level,
         );
 
-        // Loop to print burst dmg against each enemy champion
-        // for i in 0..opponant_team.opponants.len() {
-        //     let r = dmg::Resistance::new(resistance.armor[i], resistance.magic_resist[i]);
-        //     println!(
-        //         "Burst is {:.1} vs {}",
-        //         dmg::burst_dmg(&champion, &active_player_data, &ability_ranks, r),
-        //         opponant_team.opponants[i].0
-        //     );
-        // }
-        let display_data = display_data(&champion, &active_player_data, &ability_ranks, opponant_team, resistance);
-        terminal.draw(|f| ui(f, &mut app))?;
+        // let display_data = ;
 
-        app.items = vec![
-            vec![display_data[0].0.as_str(), stringify!(display_data[0].1), stringify!(display_data[0].2)],
-            vec!["Row21", "Row22", "Row23"],
-            vec!["Row31", "Row32", "Row33"],
-            vec!["Row41", "Row42", "Row43"],
-            vec!["Row51", "Row52", "Row53"],
-        ];
+        app.items = display_data(
+            &champion,
+            active_player_data,
+            ability_ranks,
+            opponant_team,
+            resistance,
+        );
         // println!("================================");
 
         // Sleep for 5 seconds between running the loop again to save resources
@@ -249,11 +249,23 @@ async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App<'_>) -> io
         //         .unwrap_or(15),
         // ))
         // .await;
+        terminal.draw(|f| ui(f, &mut app))?;
 
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => return Ok(()),
-                _ => {}
+        // if let Event::Key(key) = event::read()? {
+        //     match key.code {
+        //         KeyCode::Char('q') => return Ok(()),
+        //         _ => {}
+        //     }
+        // }
+
+        if crossterm::event::poll(Duration::from_millis(
+            env::var("SAMPLE_RATE").unwrap().parse::<u64>().unwrap(),
+        ))? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::F(12) => return Ok(()),
+                    _ => {}
+                }
             }
         }
     }
@@ -262,14 +274,13 @@ async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App<'_>) -> io
 fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let rects = Layout::default()
         .constraints([Constraint::Percentage(100)].as_ref())
-        .margin(0)
         .split(f.size());
 
     let selected_style = Style::default().add_modifier(Modifier::REVERSED);
     let normal_style = Style::default().bg(Color::Blue);
     let header_cells = ["Champion", "Level", "Burst"]
         .iter()
-        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::DarkGray)));
     let header = Row::new(header_cells)
         .style(normal_style)
         .height(1)
@@ -281,7 +292,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
             .max()
             .unwrap_or(0)
             + 1;
-        let cells = item.iter().map(|c| Cell::from(*c));
+        let cells = item.iter().map(|c| Cell::from(c.as_str()));
         Row::new(cells).height(height as u16).bottom_margin(1)
     });
     let t = Table::new(rows)
@@ -290,9 +301,24 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         .highlight_style(selected_style)
         .highlight_symbol(">> ")
         .widths(&[
-            Constraint::Percentage(50),
-            Constraint::Length(30),
-            Constraint::Min(10),
+            Constraint::Length(12),
+            Constraint::Length(5),
+            Constraint::Length(5),
         ]);
     f.render_stateful_widget(t, rects[0], &mut app.state);
+    let tui_w: TuiLoggerWidget = TuiLoggerWidget::default()
+        .block(
+            Block::default()
+                .title("Independent Tui Logger View")
+                .border_style(Style::default().fg(Color::White).bg(Color::Black))
+                .borders(Borders::ALL),
+        )
+        .output_separator('|')
+        .output_timestamp(Some("%F %H:%M:%S%.3f".to_string()))
+        .output_level(Some(TuiLoggerLevelOutput::Long))
+        .output_target(false)
+        .output_file(false)
+        .output_line(false)
+        .style(Style::default().fg(Color::White).bg(Color::Black));
+    f.render_widget(tui_w, rects[1]);
 }
